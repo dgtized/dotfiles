d6a30c6104bc96104e24b312002ab457fc092a0e
Charles Comstock <dgtized@gmail.com>
2010-09-16 03:19:59 -0500

    whitespace for setup.sh and added eclim script

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 
 function main () {
     echo "* running setup.sh from $DOTC_DIR for $DOTC_NAME"
-    
+
     echo "export DOTC_DIR=~/$DOTC_DIR" > site-config
     echo "export DOTC_NAME=$DOTC_NAME" >> site-config
 
     ln -sfv ${DOTC_DIR}/site-config ~/.site-config
-    
+
     ln -sfv ${DOTC_DIR}/bashrc ~/.bashrc
     ln -sfv ${DOTC_DIR}/emacs ~/.emacs
     ln -sfv ${DOTC_DIR}/vimrc ~/.vimrc
@@ -16,16 +16,16 @@ function main () {
     ln -sfv ${DOTC_DIR}/inputrc ~/.inputrc
     ln -sfv ${DOTC_DIR}/screenrc ~/.screenrc
     if [[ -d ~/.subversion ]]; then
-	    ln -sfv ../${DOTC_DIR}/svn-config ~/.subversion/config
+            ln -sfv ../${DOTC_DIR}/svn-config ~/.subversion/config
     fi
 
     ln -sfv ${DOTC_DIR}/pinerc ~/.pinerc
     ln -sfv ${DOTC_DIR}/muttrc ~/.muttrc
-    
+
     if [[ $DOTC_NAME == "cec" || $DOTC_NAME == "cse" ]]; then
-	ln -sfv ${DOTC_DIR}/cshrc.mine-${DOTC_NAME} ~/.cshrc.mine
+        ln -sfv ${DOTC_DIR}/cshrc.mine-${DOTC_NAME} ~/.cshrc.mine
     fi
-    
+
     # ssh related config
     mkdir -pv -m 700 $HOME/.ssh
     chmod 600 ${HOME}/${DOTC_DIR}/ssh/*
@@ -41,24 +41,24 @@ function main () {
     rm -rf site-lisp/groovy && svn export http://svn.codehaus.org/groovy/trunk/groovy/ide/emacs site-lisp/groovy
     echo "Compiling site-lisp... (see site-lisp/compile.log for detail)"
     (emacs -L site-lisp -batch -f batch-byte-compile \
-	site-lisp/*.el site-lisp/*/*.el ~/.emacs 2>&1) > site-lisp/compile.log
-    
+        site-lisp/*.el site-lisp/*/*.el ~/.emacs 2>&1) > site-lisp/compile.log
+
     mkdir -pv $HOME/.bashist
 
     # lets get some bash completion if we don't have it
     if [[ ! -f /etc/bash_completion ]]; then
-	if wget -N "http://www.caliban.org/files/bash/bash-completion-latest.tar.gz"; then
-	    tar xzf bash-completion-latest.tar.gz    
-	fi
-    fi    
+        if wget -N "http://www.caliban.org/files/bash/bash-completion-latest.tar.gz"; then
+            tar xzf bash-completion-latest.tar.gz
+        fi
+    fi
     mkdir -pv $HOME/usr/bin
     for script in `find scripts -type f | grep -v .svn`; do
-	ln -sfv ../../${DOTC_DIR}/$script ~/usr/bin;
+        ln -sfv ../../${DOTC_DIR}/$script ~/usr/bin;
     done
     if [[ -d $HOME/.scripts/ ]]; then
-    	for script in `find ${HOME}/.scripts -type f | grep -v .svn`; do
-		ln -sfv ../../$script ~/usr/bin;
-	done
+        for script in `find ${HOME}/.scripts -type f | grep -v .svn`; do
+                ln -sfv ../../$script ~/usr/bin;
+        done
     fi
     # this would be cool but then it forgets where it's from
     #ln -sfv ${DOTC_DIR}/setup.sh ~/usr/bin/home-config.sh
@@ -66,25 +66,25 @@ function main () {
 
 function valid_name () {
     until [[ $DOTC_NAME == "bio" ||
-	     $DOTC_NAME == "gentoo" ||
-	     $DOTC_NAME == "debian" ||
-	     $DOTC_NAME == "cse" ||
-	     $DOTC_NAME == "cec" ||
-	     $DOTC_NAME == "dreamhost" ]]; do
-	echo "DOTC_NAME cannot be [$DOTC_NAME], what is it? "
-	read -e var
-	eval "DOTC_NAME=\$var;"
+             $DOTC_NAME == "gentoo" ||
+             $DOTC_NAME == "debian" ||
+             $DOTC_NAME == "cse" ||
+             $DOTC_NAME == "cec" ||
+             $DOTC_NAME == "dreamhost" ]]; do
+        echo "DOTC_NAME cannot be [$DOTC_NAME], what is it? "
+        read -e var
+        eval "DOTC_NAME=\$var;"
     done
 }
 
 if [[ $# -ne 1 ]]; then
     if [ -f /etc/gentoo-release ]; then
-	DOTC_NAME="gentoo"
+        DOTC_NAME="gentoo"
     elif [ -f /etc/debian_version ]; then
-	DOTC_NAME="debian"
+        DOTC_NAME="debian"
     else
-	echo "Usage: setup.sh config-name"
-	exit
+        echo "Usage: setup.sh config-name"
+        exit
     fi
 else
     DOTC_NAME=$1
@@ -100,42 +100,42 @@ if [[ -d $HOME/$DOTC_DIR ]]; then
     cd $HOME/$DOTC_DIR
 
     if [[ $1 == "clean" ]]; then
-	echo "Cleaning..."
-	rm -rfv bash-completion-latest.tar.gz bash_completion site-config \
-	    *~ *\# site-lisp/*.elc site-lisp/compile.log
-	exit
+        echo "Cleaning..."
+        rm -rfv bash-completion-latest.tar.gz bash_completion site-config \
+            *~ *\# site-lisp/*.elc site-lisp/compile.log
+        exit
     elif [[ $1 == "up" || $1 == "update" ]]; then
         echo "Updating Configuration..."
 
-	branch=`git branch | grep '*' | sed -e 's/\* //g'`
-	echo " * saving local changes to stash"
-	git stash save
-	if [[ $branch != "master" ]]; then
-	    echo " * switching from $branch to master"
-	    git checkout master
-	fi	
-	echo " * pulling remote"
-	git pull
-	if [[ $branch != "master" ]]; then
-	    echo " * returning to $branch"
-	    git checkout $branch
-	fi
-	echo " * reapplying local changes"
-	git stash pop 
-	unset branch
-        
-	test -e site-config && source site-config
-	valid_name
-	echo "Reloading setup.sh in case of remote change"
-	exec ./setup.sh ${DOTC_NAME}
+        branch=`git branch | grep '*' | sed -e 's/\* //g'`
+        echo " * saving local changes to stash"
+        git stash save
+        if [[ $branch != "master" ]]; then
+            echo " * switching from $branch to master"
+            git checkout master
+        fi
+        echo " * pulling remote"
+        git pull
+        if [[ $branch != "master" ]]; then
+            echo " * returning to $branch"
+            git checkout $branch
+        fi
+        echo " * reapplying local changes"
+        git stash pop
+        unset branch
+
+        test -e site-config && source site-config
+        valid_name
+        echo "Reloading setup.sh in case of remote change"
+        exec ./setup.sh ${DOTC_NAME}
     fi
 
     valid_name
-    source color-bash    
+    source color-bash
 
     main # now that we know everything will get cleaned up
 
-    echo 
+    echo
     echo "Site Config for `hostname`-${DOTC_NAME}:"
     cat site-config
 else
