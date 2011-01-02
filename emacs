bf18cbe5f593e93c3b256a60c4c904835ebebede
Charles Comstock <dgtized@gmail.com>
2011-01-01 22:17:41 -0600

    added back in magit as elpa is not up to date

--- emacs
+++ emacs
@@ -23,6 +23,7 @@
 (setq custom-file (concat dotc-dir "custom.el"))
 
 (add-to-list 'load-path site-lisp)
+(add-to-list 'load-path (concat site-lisp "/vendor/magit"))
 (add-to-list 'load-path (concat site-lisp "/groovy"))
 (add-to-list 'load-path (concat site-lisp "/malabar-1.5-SNAPSHOT/lisp"))
 (add-to-list 'load-path (concat site-lisp "/emacs-eclim"))
--- setup.sh
+++ setup.sh
@@ -36,13 +36,17 @@ function main () {
 
     # These happen here so they happen after setup.sh reload
     git submodule update --init
+    pushd site-lisp/vendor/magit
+        make
+    popd
+
     wget http://mihai.bazon.net/projects/editing-javascript-with-emacs-js2-mode/js2-highlight-vars-mode/js2-highlight-vars.el\
         -O site-lisp/vendor/js2-highlight-vars.el
     rm -rf site-lisp/groovy &&\
         svn export http://svn.codehaus.org/groovy/trunk/groovy/ide/emacs site-lisp/groovy
     echo "Compiling site-lisp... (see site-lisp/compile.log for detail)"
     (emacs -L site-lisp -batch -f batch-byte-compile \
-        {site-lisp,site-lisp/vendor}/*.el site-lisp/vendor/*/*.el 2>&1) > site-lisp/compile.log
+        {site-lisp,site-lisp/vendor}/*.el 2>&1) > site-lisp/compile.log
 
     mkdir -pv $HOME/.bashist
 
