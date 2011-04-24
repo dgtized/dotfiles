b3f080634d2b8841309c913b4bbecc22a67d074d
Charles Comstock <clgc@nocturnal.(none)>
2011-04-24 18:23:00 -0500

    basically switch to using starter kit's elpa installer

--- site-lisp/clgc-elpa.el
+++ site-lisp/clgc-elpa.el
@@ -8,7 +8,7 @@
   (list 'haml-mode 'yaml-mode 'js2-mode 'sass-mode 'css-mode
         'magit 'clojure-mode 'clojure-test-mode 'paredit))
 
-(defun clgc-elpa-install ()
+(defun starter-kit-elpa-install ()
   "Install all starter-kit packages that aren't installed."
   (interactive)
   (dolist (package clgc-packages)
@@ -16,6 +16,25 @@
                 (functionp package))
       (message "Installing %s" (symbol-name package))
       (package-install package))))
+
+(defun esk-online? ()
+  "See if we're online.
+
+Windows does not have the network-interface-list function, so we
+just have to assume it's online."
+  ;; TODO how could this work on Windows?
+  (if (and (functionp 'network-interface-list)
+           (network-interface-list))
+      (some (lambda (iface) (unless (equal "lo" (car iface))
+                         (member 'up (first (last (network-interface-info
+                                                   (car iface)))))))
+            (network-interface-list))
+    t))
+
+;; On your first run, this should pull in all the base packages.
+(when (esk-online?)
+  (unless package-archive-contents (package-refresh-contents))
+  (starter-kit-elpa-install))
 
 (defun regen-autoloads (&optional force-regen)
   "Regenerate the autoload definitions file if necessary and load it."
@@ -30,5 +49,8 @@
       (let (emacs-lisp-mode-hook)
         (update-directory-autoloads autoload-dir))))
   (load autoload-file))
+
+(autoload 'paredit-mode "paredit" "" t)
+(autoload 'yaml-mode "yaml-mode" "" t)
 
 (provide 'clgc-elpa)
