d26200e2e3160f39643341aaff47a0dbcbb0a31c
Charles Comstock <dgtized@gmail.com>
2012-11-07 17:05:11 -0600

    bump clj-stacktraces

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:plugins [[lein-vanity "0.1.0"]
                   [jonase/eastwood "0.0.2"]
                   [lein-swank "1.4.4"]]
-        :dependencies {clj-stacktrace "0.2.4"}
+        :dependencies {clj-stacktrace "0.2.5"}
         :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
                                             'print-cause-trace)
                            new (ns-resolve (doto 'clj-stacktrace.repl require)
