ec5707ee2f618794b86d1787dd17f9131379373c
Charles Comstock <dgtized@gmail.com>
2012-11-07 20:14:04 -0600

    correctly autoload

--- site-lisp/clgc-lisp.el
+++ site-lisp/clgc-lisp.el
@@ -16,7 +16,7 @@
   (define-key emacs-lisp-mode-map (kbd "C-c v") 'eval-buffer)
 
   (add-to-list 'load-path "~/code/4clj-el")
-  (autoload 4clojure-problem "four-clj" "4Clojure-Mode" t)
+  (autoload '4clojure-problem "four-clj" "4Clojure-Mode" t)
 
 ;;; Enhance Lisp Modes
 
