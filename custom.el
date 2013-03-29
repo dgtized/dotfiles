066455354be4833545c4b52c654c40aacf55c46f
Charles Comstock <dgtized@gmail.com>
2013-03-29 17:34:28 -0500

    some theme updated magit handling

--- custom.el
+++ custom.el
@@ -16,5 +16,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-item-highlight ((t (:inherit highlight :underline nil)))))
+ '(magit-item-highlight ((t (:inherit highlight :underline nil))) t))
 
