a991b064b629fbc961cbe7a67300cbd9f8c38e34
Charles Comstock <dgtized@gmail.com>
2013-06-16 23:24:43 -0500

    added keybindings for line / word window diff

--- site-lisp/clgc-keybindings.el
+++ site-lisp/clgc-keybindings.el
@@ -21,6 +21,8 @@
 
 (global-set-key "\C-cr" 'rename-file-and-buffer)
 (global-set-key "\C-ct" 'visit-term-buffer)
+(global-set-key "\C-cd" 'ediff-windows-linewise)
+(global-set-key "\C-cD" 'ediff-windows-wordwise)
 
 (global-set-key [remap find-tag] 'ido-find-tag)
 (global-set-key (kbd "C-.") 'find-file-in-project)
