d5b18ef2f6b0d485f0ea3c918ebb32e4a1d258ea
Charles Comstock <dgtized@gmail.com>
2013-08-06 02:46:25 -0500

    remove javert and groovy-mode submodules

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -4,10 +4,8 @@
             [lein-ritz "0.7.0"]
             [com.palletops/pallet-lein "0.6.0-beta.7"]]
   :dependencies [[ritz/ritz-nrepl-middleware "0.7.0"]
-                 [slamhound "1.3.3"]
-                 [javert "0.2.0-SNAPSHOT"]]
+                 [slamhound "1.3.3"]]
   :repl-options {:nrepl-middleware
-                 [inspector.middleware/wrap-inspect
-                  ritz.nrepl.middleware.javadoc/wrap-javadoc
+                 [ritz.nrepl.middleware.javadoc/wrap-javadoc
                   ritz.nrepl.middleware.simple-complete/wrap-simple-complete]}
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
