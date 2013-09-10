9f01267a8ec464c24c12e20463d2dea02cf81775
Charles Comstock <dgtized@gmail.com>
2013-09-10 18:02:40 -0500

    extract major mode configuration into another lib

---
+++ site-lisp/clgc-major-modes.el
@@ -0,0 +1,52 @@
+
+;;; make Groovy mode electric by default.
+(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+(add-hook 'groovy-mode-hook
+          '(lambda ()
+             (setq c-basic-offset 4)
+             (require 'groovy-electric)
+             (groovy-electric-mode)))
+
+;; Perl Stuff (for the horrible times when I can't use ruby)
+(defalias 'perl-mode 'cperl-mode)
+(defun my-cperl-mode-hook ()
+  (setq cperl-hairy t)
+  (setq cperl-electric-keywords nil)
+  ;(define-key cperl-mode-map "\C-cp" 'cperl-perldoc)
+  ;(make-variable-buffer-local 'compile-command)
+  ;(setq compile-command (concat "perl -w " (buffer-file-name) " "))
+  )
+
+(add-hook 'cperl-mode-hook 'my-cperl-mode-hook)
+
+;;
+;; set c/c++ indent width and compile modes
+;;
+(defun my-c-mode-hook ()
+  (c-set-style "K&R")
+  (local-set-key "\C-cc" 'compile)
+)
+(add-hook 'c-mode-hook 'my-c-mode-hook)
+
+(defun my-text-mode-hook ()
+  (flyspell-mode)
+  (auto-fill-mode))
+(add-hook 'text-mode 'my-text-mode-hook)
+
+
+(add-to-list 'magic-mode-alist '("^>\\|ID\\|LOCUS\\|DNA" . dna-mode))
+(add-to-list
+ 'auto-mode-alist
+ '("\\.\\(fasta\\|fa\\|exp\\|ace\\|gb\\)\\'" . dna-mode))
+(add-hook 'dna-mode-hook 'turn-on-font-lock)
+
+(defun auctex nil
+  (interactive)
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/auctex/")
+  (load "auctex.el" nil t t)
+  (load "preview-latex.el" nil t t)
+  (setq-default TeX-master nil)
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t))
+
+(provide 'clgc-major-modes)
--- site-lisp/init.el
+++ site-lisp/init.el
@@ -33,6 +33,7 @@
 (require 'clgc-find-files)
 (require 'starter-kit-eshell)
 (require 'clgc-ruby)
+(require 'clgc-major-modes)
 
 (setq inhibit-startup-screen t
       line-number-mode t
@@ -94,41 +95,6 @@
 (setq compilation-read-command t)
 (setq compilation-window-height 12)
 
-;;; make Groovy mode electric by default.
-(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
-(add-hook 'groovy-mode-hook
-          '(lambda ()
-             (setq c-basic-offset 4)
-             (require 'groovy-electric)
-             (groovy-electric-mode)))
-
-;; Perl Stuff (for the horrible times when I can't use ruby)
-(defalias 'perl-mode 'cperl-mode)
-(defun my-cperl-mode-hook ()
-  (setq cperl-hairy t)
-  ;(setq cperl-auto-newline t)
-  (setq cperl-electric-keywords nil)
-  ;(define-key cperl-mode-map "\C-cp" 'cperl-perldoc)
-  ;(make-variable-buffer-local 'compile-command)
-  ;(setq compile-command (concat "perl -w " (buffer-file-name) " "))
- )
-
-(add-hook 'cperl-mode-hook 'my-cperl-mode-hook)
-
-;;
-;; set c/c++ indent width and compile modes
-;;
-(defun my-c-mode-hook ()
-  (c-set-style "K&R")
-  (local-set-key "\C-cc" 'compile)
-)
-(add-hook 'c-mode-hook 'my-c-mode-hook)
-
-(defun my-text-mode-hook ()
-  (flyspell-mode)
-  (auto-fill-mode))
-(add-hook 'text-mode 'my-text-mode-hook)
-
 (defun toggle-fullscreen (&optional f)
   (interactive)
   (let ((current-value (frame-parameter nil 'fullscreen)))
@@ -143,23 +109,7 @@
 (require 'generic-x)
 (require 'page-ext)
 
-(add-to-list 'magic-mode-alist '("^>\\|ID\\|LOCUS\\|DNA" . dna-mode))
-(add-to-list
- 'auto-mode-alist
- '("\\.\\(fasta\\|fa\\|exp\\|ace\\|gb\\)\\'" . dna-mode))
-(add-hook 'dna-mode-hook 'turn-on-font-lock)
-
-(defun auctex nil
-  (interactive)
-  (add-to-list 'load-path "/usr/share/emacs/site-lisp/auctex/")
-  (load "auctex.el" nil t t)
-  (load "preview-latex.el" nil t t)
-  (setq-default TeX-master nil)
-  (setq TeX-auto-save t)
-  (setq TeX-parse-self t))
-
 ;; http://repose.cx/conf/.elisp/
-
 ;; Or enable more if you wish
 (setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
                   global-semanticdb-minor-mode
