e09cb4a4143031a5270ec1544e85102981e6dc06
Charles Comstock <dgtized@gmail.com>
2013-10-04 18:05:18 -0500

    version bump pallet-lein 0.8.0-alpha.1

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -2,7 +2,7 @@
  {:plugins [[lein-vanity "0.1.0"]
             [jonase/eastwood "0.0.2"]
             [lein-ritz "0.7.0"]
-            [com.palletops/pallet-lein "0.6.0-beta.7"]]
+            [com.palletops/pallet-lein "0.8.0-alpha.1"]]
   :dependencies [[ritz/ritz-nrepl-middleware "0.7.0"]
                  [slamhound "1.3.3"]]
   :repl-options {:nrepl-middleware
