db2dc41bd4dca482ff7650cfb05f44940d9f926a
Charles Comstock <dgtized@gmail.com>
2013-10-12 18:20:16 -0500

    Convert to using kbd macro for binding

--- site-lisp/clgc-keybindings.el
+++ site-lisp/clgc-keybindings.el
@@ -1,38 +1,38 @@
 (global-set-key [f11] 'toggle-fullscreen)
 
-(global-set-key "\C-xO" (lambda () (interactive) (other-window -1)))
-(global-set-key "\C-xp" (lambda () (interactive) (other-window -1)))
+(global-set-key (kbd "C-x O") (lambda () (interactive) (other-window -1)))
+(global-set-key (kbd "C-x p") (lambda () (interactive) (other-window -1)))
 
-(global-set-key "\C-xE" 'apply-macro-to-region-lines)
+(global-set-key (kbd "C-x E") 'apply-macro-to-region-lines)
 
 ;;so now Control-c 7 prompts for a Unicode hex code, will then insert the glyph
-(global-set-key "\C-c7" 'insert-char)
+(global-set-key (kbd "C-c 7") 'insert-char)
 
-(global-set-key "\C-xg" 'magit-status)
+(global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key [f6] 'magit-status)
 (global-set-key [f8] 'eshell)
 
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c\C-m" 'execute-extended-command)
+(global-set-key (kbd "C-x C-m") 'execute-extended-command)
+(global-set-key (kbd "C-c C-m") 'execute-extended-command)
 
-(global-set-key "\C-x\C-b" 'ibuffer)
-(global-set-key "\M-/" 'hippie-expand)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "M-/") 'hippie-expand)
 
-(global-set-key "\C-ck" 'kill-whole-line)
+(global-set-key (kbd "C-c k") 'kill-whole-line)
 (global-set-key (kbd "C-c j") 'just-one-space)
-(global-set-key "\C-cw" 'whitespace-cleanup)
-(global-set-key "\C-cq" 'comment-dwim)
+(global-set-key (kbd "C-c w") 'whitespace-cleanup)
+(global-set-key (kbd "C-c q") 'comment-dwim)
 
-(global-set-key "\C-cR" 'rename-file-and-buffer)
-(global-set-key "\C-ct" 'visit-term-buffer)
-(global-set-key "\C-cd" 'ediff-windows-linewise)
-(global-set-key "\C-cD" 'ediff-windows-wordwise)
-(global-set-key "\C-cs" 'isearch-other-window)
+(global-set-key (kbd "C-c R") 'rename-file-and-buffer)
+(global-set-key (kbd "C-c t") 'visit-term-buffer)
+(global-set-key (kbd "C-c d") 'ediff-windows-linewise)
+(global-set-key (kbd "C-c D") 'ediff-windows-wordwise)
+(global-set-key (kbd "C-c s") 'isearch-other-window)
 
 (global-set-key (kbd "C-.") 'find-file-in-project)
 (global-set-key (kbd "C-,") 'find-grep-in-project)
 
-(global-set-key "\M-T" 'transpose-sexps)
+(global-set-key (kbd "M-T") 'transpose-sexps)
 
 (global-set-key (kbd "<f9>") 'org-tree-slide-mode)
 (global-set-key (kbd "M-<f9>") 'org-tree-slide-mode)
