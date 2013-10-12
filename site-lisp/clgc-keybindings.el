3c14d62c56e1174ec9405c6d283cff0ef5bf67d0
Charles Comstock <dgtized@gmail.com>
2013-10-12 18:39:18 -0500

    Default to regexp search/replace

--- site-lisp/clgc-keybindings.el
+++ site-lisp/clgc-keybindings.el
@@ -39,6 +39,11 @@
 
 (global-set-key (kbd "C-=") 'er/expand-region)
 
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
 (windmove-default-keybindings)
 
 ;; isearch
