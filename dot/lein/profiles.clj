63b10b1a658be52a78655d2b21495808621cff48
Charles Comstock <dgtized@gmail.com>
2013-10-27 18:15:26 -0500

    bump lein-vanity to 0.2.0

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user
- {:plugins [[lein-vanity "0.1.0"]
+ {:plugins [[lein-vanity "0.2.0"]
             [jonase/eastwood "0.0.2"]
             ;[lein-ritz "0.7.0"]
             [com.palletops/pallet-lein "0.8.0-alpha.1"]]
