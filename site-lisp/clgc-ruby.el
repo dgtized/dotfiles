99a27ec932b9288b181718b77417a6d6c246cafc
Charles Comstock <dgtized@gmail.com>
2013-12-20 03:00:46 -0600

    disable ac completion for robe

--- site-lisp/clgc-ruby.el
+++ site-lisp/clgc-ruby.el
@@ -53,7 +53,9 @@
 
 ;; Robe
 (add-hook 'ruby-mode-hook 'robe-mode)
-(add-hook 'robe-mode 'robe-ac-setup)
+
+;; broken per https://github.com/dgutov/robe/issues/20
+;; (add-hook 'robe-mode 'robe-ac-setup)
 
 (eval-after-load 'rspec-mode
   '(rspec-install-snippets))
