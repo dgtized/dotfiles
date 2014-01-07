f17dfaa3bf543b674657a06a624dd82159f838c9
Charles Comstock <dgtized@gmail.com>
2014-01-07 05:08:05 -0600

    Added web-mode

--- site-lisp/clgc-major-modes.el
+++ site-lisp/clgc-major-modes.el
@@ -33,6 +33,15 @@
   (auto-fill-mode))
 (add-hook 'text-mode 'my-text-mode-hook)
 
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 (add-to-list 'magic-mode-alist '("^>\\|ID\\|LOCUS\\|DNA" . dna-mode))
 (add-to-list
