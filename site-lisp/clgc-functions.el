0b1dc0430eddebf5150c5284f87f1830987a1f55
Charles Comstock <dgtized@gmail.com>
2014-01-08 09:58:47 -0600

    Add view-url
    Too bad it's not a full blown mode

--- site-lisp/clgc-functions.el
+++ site-lisp/clgc-functions.el
@@ -129,6 +129,17 @@ With negative N, comment out original line and use the absolute value."
         (message "Setting %s to %s" var value)
         (setenv var value)))))
 
+(defun view-url ()
+  "Open a new buffer containing the contents of URL."
+  (interactive)
+  (let* ((default (thing-at-point-url-at-point))
+         (url (read-from-minibuffer "URL: " default)))
+    (switch-to-buffer (url-retrieve-synchronously url))
+    (rename-buffer url t)
+    ;; TODO: switch to nxml/nxhtml mode
+    (cond ((search-forward "<?xml" nil t) (xml-mode))
+          ((search-forward "<html" nil t) (html-mode)))))
+
 (defun github-browse ()
   (interactive)
   (let ((url (magit-get "remote" (magit-get-current-remote) "url")))
