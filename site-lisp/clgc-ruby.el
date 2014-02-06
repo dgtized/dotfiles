32423b34003f9d721dde3b74b1de12be51ed677f
Charles Comstock <dgtized@gmail.com>
2014-02-06 15:29:24 -0600

    nuke ruby toggle string type for the moment

--- site-lisp/clgc-ruby.el
+++ site-lisp/clgc-ruby.el
@@ -2,46 +2,13 @@
    "Evaluate the buffer with ruby."
    (shell-command-on-region (point-min) (point-max) "ruby -w "))
 
-(eval-when-compile (require 'ruby-tools))
-(defun ruby-tools-jump-to-inside-of-thing ()
-  ;; jump out of back or into beginning of string/symbol
-  (when (looking-at "[:'\"]") (forward-char))
-  ;; jump into beginning of string/symbol if after
-  (when (not (or (ruby-tools-symbol-at-point-p)
-                 (ruby-tools-string-at-point-p)))
-    (ruby-backward-sexp))
-  ;; but we still need to be inside to trigger the toggle
-  (when (looking-at "[:'\"]") (forward-char)))
-
-(defun ruby-toggle-symbol-string ()
-  (interactive)
-  (save-excursion
-    (ruby-tools-jump-to-inside-of-thing)
-    (cond ((ruby-tools-symbol-at-point-p)
-           (ruby-tools-to-double-quote-string))
-          ((ruby-tools-string-at-point-p)
-           (ruby-tools-to-symbol)))))
-
-(defun ruby-toggle-string-type ()
-  (interactive)
-  (save-excursion
-    (ruby-tools-jump-to-inside-of-thing)
-    (if (looking-back "[':]" (1- (point)))
-        (ruby-tools-to-double-quote-string)
-      (ruby-tools-to-single-quote-string))))
-
 (defun my-ruby-mode-hook ()
   ;; Keep ac-mode from trying to complete on an end
   (make-local-variable 'ac-ignores)
   (add-to-list 'ac-ignores "end")
   (require 'rinari)
   (setq rinari-tags-file-name "TAGS")
-  (global-rinari-mode t)
-
-  (require 'ruby-tools)
-  (define-key ruby-tools-mode-map (kbd "C-:") 'ruby-toggle-symbol-string)
-  (define-key ruby-tools-mode-map (kbd "C-\"") 'ruby-toggle-string-type)
-  (define-key ruby-tools-mode-map (kbd "C-'") nil))
+  (global-rinari-mode t))
 
 (add-hook 'ruby-mode-hook 'my-ruby-mode-hook)
 (add-hook 'ruby-mode-hook 'subword-mode)
