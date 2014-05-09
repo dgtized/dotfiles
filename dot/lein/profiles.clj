d0b2b54fd80e88feecc23937595fc122b508d887
Charles Comstock <dgtized@gmail.com>
2014-05-09 16:22:50 -0500

    Add cider-nrepl middleware

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user
  {:plugins [[lein-vanity "0.2.0"]
             [jonase/eastwood "0.0.2"]
+            [cider/cider-nrepl "0.7.0-SNAPSHOT"]
             ;[lein-ritz "0.7.0"]
             [com.palletops/pallet-lein "0.8.0-alpha.1"]]
   :dependencies [;[ritz/ritz-nrepl-middleware "0.7.0"]
