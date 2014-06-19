ced096229febcfaf657331cb4e843e267b4c884c
Charles Comstock <dgtized@gmail.com>
2014-06-19 03:30:52 -0500

    Bump eastwood version to 0.1.3

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user
  {:plugins [[lein-vanity "0.2.0"]
-            [jonase/eastwood "0.1.2"]
+            [jonase/eastwood "0.1.3"]
             [cider/cider-nrepl "0.7.0-SNAPSHOT"]
             [com.palletops/pallet-lein "0.8.0-alpha.1"]]
   :dependencies [[slamhound "1.5.0"]]
