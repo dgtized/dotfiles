98dc5acaaead829bd357b60e121c6c21c7e43401
Charles Comstock <dgtized@gmail.com>
2014-07-25 19:42:44 -0500

    Cider is more stable these days

--- site-lisp/clgc-lisp.el
+++ site-lisp/clgc-lisp.el
@@ -1,5 +1,4 @@
 (progn
-  (require 'cider) ;; until cider autoloads are stable
   (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
   (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
   (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
