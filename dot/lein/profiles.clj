e89e66e53046e43ed3867170043d793c62c45a9e
Charles Comstock <dgtized@gmail.com>
2014-08-09 13:16:04 -0500

    Bump cider-nrepl dependency

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user
  {:plugins [[lein-vanity "0.2.0"]
             [jonase/eastwood "0.1.3"]
-            [cider/cider-nrepl "0.7.0-SNAPSHOT"]
+            [cider/cider-nrepl "0.8.0-SNAPSHOT"]
             [com.palletops/pallet-lein "0.8.0-alpha.1"]]
   :dependencies [[slamhound "1.5.0"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
