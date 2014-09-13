8fc60e14167a1162f374c4e46e391bc13c6fb2ac
Charles Comstock <dgtized@gmail.com>
2014-09-13 17:10:10 -0500

    Add lein-ancient

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user
  {:plugins [[lein-vanity "0.2.0"]
+            [lein-ancient "0.5.5"]
             [jonase/eastwood "0.1.4"]
             [cider/cider-nrepl "0.8.0-SNAPSHOT"]]
   :dependencies [[slamhound "1.5.0"]]
