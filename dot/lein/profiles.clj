4ecad16a9e60f5e1b7f90418f57be9473cc7a231
Charles Comstock <dgtized@gmail.com>
2014-12-11 02:44:58 -0600

    Bump basic clojure profile dependencies

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user
  {:plugins [[lein-vanity "0.2.0"]
             [lein-ancient "0.5.5"]
-            [jonase/eastwood "0.1.4"]
-            [cider/cider-nrepl "0.8.0-SNAPSHOT"]]
+            [jonase/eastwood "0.2.0"]
+            [cider/cider-nrepl "0.8.1"]]
   :dependencies [[slamhound "1.5.5"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
