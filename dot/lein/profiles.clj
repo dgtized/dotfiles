0fea980e85e194155c05d6a091ef08debfdca15d
Charles Comstock <dgtized@gmail.com>
2014-12-22 16:15:08 -0600

    cider-nrepl 0.8.2

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -2,6 +2,6 @@
  {:plugins [[lein-vanity "0.2.0"]
             [lein-ancient "0.5.5"]
             [jonase/eastwood "0.2.0"]
-            [cider/cider-nrepl "0.8.1"]]
+            [cider/cider-nrepl "0.8.2"]]
   :dependencies [[slamhound "1.5.5"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
