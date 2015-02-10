e3a425b6aa60ddbf15c0192e4c9598e685052ef4
Charles Comstock <dgtized@gmail.com>
2015-02-10 16:14:07 -0600

    Emacs provides isearch-occur with M-s o by default

--- site-lisp/clgc-keybindings.el
+++ site-lisp/clgc-keybindings.el
@@ -180,12 +180,6 @@
 (eval-after-load 'company
   '(global-set-key (kbd "C-c y") 'company-yasnippet))
 
-;; isearch
-(define-key isearch-mode-map (kbd "C-o")
-  (lambda ()
-    (interactive)
-    (let ((case-fold-search isearch-case-fold-search))
-      (occur (if isearch-regexp isearch-string
-           (regexp-quote isearch-string))))))
+(define-key isearch-mode-map (kbd "C-o") 'isearch-occur)
 
 (provide 'clgc-keybindings)
