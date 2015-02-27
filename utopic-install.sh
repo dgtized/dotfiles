660d66287cbb903e84ecc9c163177cae40b10eb7
Charles Comstock <dgtized@gmail.com>
2015-02-27 00:54:02 -0600

    utopic packages

---
+++ utopic-install.sh
@@ -0,0 +1,22 @@
+sudo apt-get install aptitude git openssh-server tmux build-essential
+
+sudo apt-add-repository ppa:ubuntu-elisp/ppa
+sudo aptitude install emacs-snapshot emacs-snapshot-el
+
+sudo aptitude install fonts-inconsolata python-wnck
+
+sudo aptitude install silversearcher-ag ack-grep
+
+sudo aptitude install mysql-client-5.6 mysql-server-5.6 libmysqlclient-dev \
+     postgresql-9.4  libpq-dev
+
+# uncomment localhost for postgres
+
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
+
+sudo aptitude install redis-server redis-tools
+
+sudo aptitude install graphviz
+
+
+
