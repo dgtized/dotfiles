3de79d6187aeab02a3bf79ab7d462600e6890807
Charles Comstock <dgtized@gmail.com>
2015-03-14 18:50:51 -0500

    reformat packages and add font-dejavu

--- utopic-install.sh
+++ utopic-install.sh
@@ -3,11 +3,12 @@ sudo apt-get install aptitude git openssh-server tmux build-essential
 sudo apt-add-repository ppa:ubuntu-elisp/ppa
 sudo aptitude update
 sudo aptitude install emacs-snapshot emacs-snapshot-el vim \
-	fonts-inconsolata python-wnck silversearcher-ag ack-grep \
-	mysql-client-5.6 mysql-server-5.6 libmysqlclient-dev \
-     	postgresql-9.4 libpq-dev redis-server redis-tools \
-	git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties \
-	graphviz dconf-cli gnome-tweak-tool
+     fonts-inconsolata fonts-dejavu \
+     python-wnck silversearcher-ag ack-grep \
+     mysql-client-5.6 mysql-server-5.6 libmysqlclient-dev \
+     postgresql-9.4 libpq-dev redis-server redis-tools \
+     git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties \
+     graphviz dconf-cli gnome-tweak-tool
 
 # uncomment localhost for postgres
 
