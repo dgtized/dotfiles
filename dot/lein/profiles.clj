560417c3ec953e40367eb5d0b410de89c91d00b7
Charles Comstock <dgtized@gmail.com>
2015-03-28 17:46:22 -0500

    Cider-nrepl and lein-ancient version bump

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user
  {:plugins [[lein-vanity "0.2.0" :exclusions [org.clojure/clojure]]
-            [lein-ancient "0.5.5"]
+            [lein-ancient "0.6.5"]
             [jonase/eastwood "0.2.0"]
-            [cider/cider-nrepl "0.8.2"]]
+            [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
   :dependencies [[slamhound "1.5.5"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
