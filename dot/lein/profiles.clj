1b135c44dd896510715f54a956b64ac3513a6d82
Charles Comstock <dgtized@gmail.com>
2015-07-04 16:40:14 -0500

    Upgrade cider dependencies

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -2,6 +2,8 @@
  {:plugins [[lein-vanity "0.2.0" :exclusions [org.clojure/clojure]]
             [lein-ancient "0.6.5"]
             [jonase/eastwood "0.2.0"]
-            [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
-  :dependencies [[slamhound "1.5.5"]]
+            [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+            [refactor-nrepl "1.1.0-SNAPSHOT"]]
+  :dependencies [[slamhound "1.5.5"]
+                 [org.clojure/tools.nrepl "0.2.10"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
