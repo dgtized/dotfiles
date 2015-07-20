89d445b71c03b4c128348c2f24ed61d889a4cf96
Charles Comstock <dgtized@gmail.com>
2015-07-19 23:15:36 -0500

    Enable eshell visual subcommands

--- site-lisp/clgc-shell.el
+++ site-lisp/clgc-shell.el
@@ -1,8 +1,13 @@
 (require 'eshell)
+(require 'em-term)
 (require 'em-smart)
 (setq eshell-where-to-jump 'begin)
 (setq eshell-review-quick-commands nil)
 (setq eshell-smart-space-goes-to-end t)
 
 (add-hook 'eshell-mode-hook 'eshell-smart-initialize)
+(add-to-list 'eshell-visual-commands "tmux")
+
+(setq eshell-visual-subcommands '(("git" "log" "diff" "show")))
+
 (provide 'clgc-shell)
