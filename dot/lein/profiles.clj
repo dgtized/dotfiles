092248ca46e8e1155b1bd2c7c17171d8c164f242
Charles Comstock <dgtized@gmail.com>
2015-08-08 20:51:40 -0500

    Bump refactor-nrepl to 1.2.0-SNAPSHOT

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -3,7 +3,7 @@
             [lein-ancient "0.6.5"]
             [jonase/eastwood "0.2.0"]
             [cider/cider-nrepl "0.10.0-SNAPSHOT"]
-            [refactor-nrepl "1.1.0-SNAPSHOT"]]
+            [refactor-nrepl "1.2.0-SNAPSHOT"]]
   :dependencies [[slamhound "1.5.5"]
                  [org.clojure/tools.nrepl "0.2.10"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
