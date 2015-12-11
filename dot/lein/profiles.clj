d5dd4b3ff11576feee501109225ad028bce91d1c
Charles Comstock <dgtized@gmail.com>
2015-12-11 02:25:38 -0600

    Chase cider versions

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -2,8 +2,8 @@
  {:plugins [[lein-vanity "0.2.0" :exclusions [org.clojure/clojure]]
             [lein-ancient "0.6.5"]
             [jonase/eastwood "0.2.0"]
-            [cider/cider-nrepl "0.10.0-SNAPSHOT"]
-            [refactor-nrepl "1.2.0-SNAPSHOT"]]
+            [cider/cider-nrepl "0.11.0-SNAPSHOT"]
+            [refactor-nrepl "2.0.0-SNAPSHOT"]]
   :dependencies [[slamhound "1.5.5"]
-                 [org.clojure/tools.nrepl "0.2.10"]]
+                 [org.clojure/tools.nrepl "0.2.12"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
