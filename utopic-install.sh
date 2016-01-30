093c8aca5fb244eec033d550da88b6b6b24f8a8b
Charles Comstock <dgtized@gmail.com>
2016-01-29 18:31:16 -0600

    Use nodejs 4.x

--- utopic-install.sh
+++ utopic-install.sh
@@ -21,7 +21,7 @@ sudo apt-get install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-beta_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 
-curl --silent --location https://deb.nodesource.com/setup_0.12 | sudo bash - && \
+curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash - && \
     sudo apt-get install --yes nodejs
 
 #rbenv install 2.1.1
