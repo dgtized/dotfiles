70557958626bb271c2394e090a808a30064c3cc2
Charles Comstock <dgtized@gmail.com>
2016-02-01 19:31:15 -0600

    bump to nodejs 5

--- utopic-install.sh
+++ utopic-install.sh
@@ -22,7 +22,7 @@ sudo apt-get install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-beta_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 
-curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash - && \
+curl --silent --location https://deb.nodesource.com/setup_5.x | sudo bash - && \
     sudo apt-get install --yes nodejs
 
 # install chruby
