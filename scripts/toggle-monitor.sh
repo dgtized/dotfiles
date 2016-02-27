279bf2a689cbb7d58f10c1feb8ffdc1ad3d813a1
Charles Comstock <dgtized@gmail.com>
2016-02-26 22:12:54 -0600

    Document toggle-monitor bug

--- scripts/toggle-monitor.sh
+++ scripts/toggle-monitor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# FIXME: changing this with xrandr isn't sticky enough. After the monitor wakes
+# back up from sleep it keeps reverting to 1920x1080 until the setting is
+# adjusted with unity-control-center display settings again
 if xrandr | grep DP-0 | grep 3840x2160; then
     xrandr --output DP-0 --mode 1920x1080 --rate 60
 else
