0931bf02fdc736fb07e0fc8cf9ba59e11a758dc8
Charles Comstock <dgtized@gmail.com>
2016-03-25 19:56:25 -0500

    refactor-nrepl 2.2.0 (no snapshot!)

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -2,7 +2,7 @@
  {:plugins [[lein-vanity "0.2.0" :exclusions [org.clojure/clojure]]
             [lein-ancient "0.6.5"]
             [jonase/eastwood "0.2.0"]
-            [refactor-nrepl "2.2.0-SNAPSHOT"]]
+            [refactor-nrepl "2.2.0"]]
   :dependencies [[slamhound "1.5.5"]
                  [org.clojure/tools.nrepl "0.2.12"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
