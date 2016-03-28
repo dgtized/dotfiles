c52165d540cf3a0e7bcb306afe5bf02481abd980
Charles Comstock <dgtized@gmail.com>
2016-03-28 16:46:11 -0500

    docker install instructions

--- utopic-install.sh
+++ utopic-install.sh
@@ -56,3 +56,13 @@ sudo service ssh restart
 # download otp from https://www.erlang.org/downloads
 # mix local.hex
 # mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
+
+# apt-get install apt-transport-https ca-certificates
+# sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+# echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" | \
+#    sudo tee /etc/apt/sources.list.d/docker.list
+# sudo apt-get update
+# sudo apt-get install linux-image-extra-$(uname -r)
+# sudo apt-get install docker-engine
+# sudo service docker start
+
