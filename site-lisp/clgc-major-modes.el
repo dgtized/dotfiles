09e4314b5eb860596d379ad3d93f7ede4266d668
Charles Comstock <dgtized@gmail.com>
2016-05-24 22:56:40 -0500

    Push alchemist configuration into an eval-after-load

--- site-lisp/clgc-major-modes.el
+++ site-lisp/clgc-major-modes.el
@@ -22,8 +22,10 @@
           elm-format-on-save t)))
 
 ;; Elixir
-(setq alchemist-goto-elixir-source-dir (expand-file-name "~/code/elixir/elixir")
-      alchemist-goto-erlang-source-dir (expand-file-name "~/code/erlang-otp"))
+(eval-after-load 'alchemist-mode
+  (progn
+    (setq alchemist-goto-elixir-source-dir (expand-file-name "~/code/elixir/elixir")
+          alchemist-goto-erlang-source-dir (expand-file-name "~/code/erlang-otp"))))
 
 ;;; make Groovy mode electric by default.
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
