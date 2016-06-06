e197e247abd7ea02c978a139e827430e30d48fc9
Charles Comstock <dgtized@gmail.com>
2016-06-05 21:07:07 -0500

    WIP elixir / smartparens integration

--- site-lisp/clgc-major-modes.el
+++ site-lisp/clgc-major-modes.el
@@ -34,6 +34,25 @@
           alchemist-hooks-compile-on-save t
           alchemist-hooks-test-on-save t)))
 
+(defun my-elixir-do-end-close-action (id action context)
+  (when (eq action 'insert)
+    (newline-and-indent)
+    (forward-line -1)
+    (indent-according-to-mode)))
+
+(sp-with-modes '(elixir-mode)
+  (sp-local-pair "fn" "end"
+                 :when '(("SPC" "RET"))
+                 :actions '(insert))
+  (sp-local-pair "->" "end"
+                 :when '(("RET"))
+                 :post-handlers '(:add my-elixir-do-end-close-action)
+                 :actions '(insert))
+  (sp-local-pair "do" "end"
+                 :when '(("SPC" "RET"))
+                 :post-handlers '(:add my-elixir-do-end-close-action)
+                 :actions '(insert)))
+
 ;;; make Groovy mode electric by default.
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
 (add-hook 'groovy-mode-hook
