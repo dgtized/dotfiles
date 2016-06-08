fa221c292da1f8268511453ef7adfa349f70ffb0
Charles Comstock <dgtized@gmail.com>
2016-06-08 13:51:54 -0500

    move agenda from a to C

--- site-lisp/clgc-keybindings.el
+++ site-lisp/clgc-keybindings.el
@@ -55,7 +55,7 @@
 
 ;; Org Related
 (global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c C") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "C-c b") 'org-switchb)
 
