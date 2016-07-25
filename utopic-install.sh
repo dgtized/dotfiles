78ea23a31cee68ca59503bd58a21d96167e28060
Charles Comstock <dgtized@gmail.com>
2016-07-25 00:25:48 -0500

    Make sure netcat is installed

--- utopic-install.sh
+++ utopic-install.sh
@@ -9,7 +9,7 @@ sudo aptitude install emacs-snapshot emacs-snapshot-el vim \
      postgresql-9.4 libpq-dev redis-server redis-tools \
      git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties \
      graphviz dconf-cli gnome-tweak-tool \
-     p7zip-full tree parallel pv gawk \
+     p7zip-full tree parallel pv gawk netcat-openbsd \
      lm-sensors htop iotop linux-tools-common linux-tools-generic \ # perf
      default-jdk \
      gitk \
