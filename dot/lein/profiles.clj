cd6d34b092da371c2d8939b45ff450ee8c2ae4f6
Charles Comstock <dgtized@gmail.com>
2016-12-03 17:42:36 -0600

    Add support for sayid, a clojure tracing tool

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user
  {:plugins [[lein-vanity "0.2.0" :exclusions [org.clojure/clojure]]
             [lein-ancient "0.6.5"]
-            [jonase/eastwood "0.2.0"]]
+            [jonase/eastwood "0.2.0"]
+            [com.billpiel/sayid "0.0.10"]]
   :dependencies [[slamhound "1.5.5"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
