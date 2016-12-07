36df035c8158a8b2254488dee16148c9426ae804
Charles Comstock <dgtized@gmail.com>
2016-12-07 00:21:42 -0600

    add an explain extend command for sql work

--- site-lisp/clgc-sql.el
+++ site-lisp/clgc-sql.el
@@ -17,4 +17,36 @@
 (defun clgc-sql-interactive-hook () (toggle-truncate-lines t))
 (add-hook 'sql-interactive-mode-hook 'clgc-sql-interactive-hook)
 
+
+;; (sql-set-product-feature 'mysql :explain "explain format=json")
+
+;;;###autoload
+(defun clgc-mysql-explain-statement ()
+  "Send the current command at point to SQL, prefixed with explain"
+  (interactive)
+  (let* ((outbuf "*SQL EXPLAIN EXTENDED*")
+         (start (save-excursion
+                  (backward-paragraph)
+                  (point)))
+         (end (save-excursion
+                (forward-paragraph)
+                (point)))
+         (command (concat "explain format=json "
+                          (buffer-substring-no-properties start end))))
+    (sql-execute sql-buffer outbuf command nil nil)
+    (with-current-buffer outbuf
+      (read-only-mode -1)
+      (goto-char (point-min))
+      (when (search-forward "{")
+        (delete-region 1 (1- (point))))
+      (goto-char (point-max))
+      (when (search-backward "}")
+        (delete-region (1+ (point)) (point-max)))
+      (goto-char (point-min))
+      (js-mode)
+      )))
+
+(define-key sql-interactive-mode-map (kbd "C-c C-e") 'clgc-mysql-explain-statement)
+(define-key sql-mode-map (kbd "C-c C-e") 'clgc-mysql-explain-statement)
+
 (provide 'clgc-sql)
