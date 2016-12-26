728496a822d32029a9c9215379857c4fdd9d3bf1
Charles Comstock <dgtized@gmail.com>
2016-12-25 21:53:56 -0600

    Unwrap from progn block

--- site-lisp/clgc-lisp.el
+++ site-lisp/clgc-lisp.el
@@ -1,51 +1,50 @@
-(progn
-  (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-  (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
-  (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
-  (add-hook 'emacs-lisp-mode-hook 'auto-recompile-el-buffer)
+(require 'clj-refactor)
 
-  (defun auto-recompile-el-buffer ()
-    "Recompile elisp buffers on save if it has been compiled before"
-    (make-local-variable 'after-save-hook)
-    (add-hook 'after-save-hook
-              (lambda ()
-                (when (file-exists-p (byte-compile-dest-file buffer-file-name))
-                  (byte-compile-file buffer-file-name)))))
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook 'auto-recompile-el-buffer)
 
-  ;; Enhance Lisp Modes
-  (dolist (mode '(scheme emacs-lisp inferior-emacs-lisp lisp clojure
-                         inferior-lisp slime slime-repl cider-repl))
-    (let ((mode-hook (intern (concat (symbol-name mode) "-mode-hook"))))
-      (progn
-        (add-hook mode-hook 'smartparens-strict-mode))))
+(defun auto-recompile-el-buffer ()
+  "Recompile elisp buffers on save if it has been compiled before"
+  (make-local-variable 'after-save-hook)
+  (add-hook 'after-save-hook
+            (lambda ()
+              (when (file-exists-p (byte-compile-dest-file buffer-file-name))
+                (byte-compile-file buffer-file-name)))))
 
-  (defadvice cider-eval-last-sexp (after cider-flash-last activate)
-    (flash-region (save-excursion (backward-sexp) (point)) (point)))
-  (defadvice cider-eval-defun-at-point (after cider-flash-at activate)
-    (apply #'flash-region (cider--region-for-defun-at-point)))
+;; Enhance Lisp Modes
+(dolist (mode '(scheme emacs-lisp inferior-emacs-lisp lisp clojure
+                       inferior-lisp slime slime-repl cider-repl))
+  (let ((mode-hook (intern (concat (symbol-name mode) "-mode-hook"))))
+    (progn
+      (add-hook mode-hook 'smartparens-strict-mode))))
 
-  ;; Clojure Specific
-  (defun clgc-clojure-mode-hook ()
-    (subword-mode +1)
-    (require 'clj-refactor)
-    (clj-refactor-mode t)
-    (cljr-add-keybindings-with-prefix "C-c r"))
+(defadvice cider-eval-last-sexp (after cider-flash-last activate)
+  (flash-region (save-excursion (backward-sexp) (point)) (point)))
+(defadvice cider-eval-defun-at-point (after cider-flash-at activate)
+  (apply #'flash-region (cider--region-for-defun-at-point)))
 
-  (defun clgc-cider-repl-mode-hook ()
-    (subword-mode +1)
-    (eldoc-mode))
+;; Clojure Specific
+(defun clgc-clojure-mode-hook ()
+  (subword-mode +1)
+  (clj-refactor-mode t)
+  (cljr-add-keybindings-with-prefix "C-c r"))
 
-  (defun clgc-cider-mode-hook ()
-    (eldoc-mode))
+(defun clgc-cider-repl-mode-hook ()
+  (subword-mode +1)
+  (eldoc-mode))
 
-  (eval-after-load 'cider
-    (setq cider-show-error-buffer nil
-          cider-prompt-for-symbol nil))
+(defun clgc-cider-mode-hook ()
+  (eldoc-mode))
 
-  (add-hook 'clojure-mode-hook 'clgc-clojure-mode-hook)
-  (add-hook 'cider-repl-mode-hook 'clgc-cider-repl-mode-hook)
-  (add-hook 'cider-mode-hook 'clgc-cider-mode-hook)
-  ;;(eval-after-load 'clojure-mode '(flycheck-clojure-setup))
-  )
+(eval-after-load 'cider
+  (setq cider-show-error-buffer nil
+        cider-prompt-for-symbol nil))
+
+(add-hook 'clojure-mode-hook 'clgc-clojure-mode-hook)
+(add-hook 'cider-repl-mode-hook 'clgc-cider-repl-mode-hook)
+(add-hook 'cider-mode-hook 'clgc-cider-mode-hook)
+;;(eval-after-load 'clojure-mode '(flycheck-clojure-setup))
 
 (provide 'clgc-lisp)
