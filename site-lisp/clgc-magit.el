88c157e5ee6045576d61a5acfa4798931fd2d823
Charles Comstock <dgtized@gmail.com>
2017-01-04 15:36:55 -0600

    Disable magit-insert-tags due to performance problems

--- site-lisp/clgc-magit.el
+++ site-lisp/clgc-magit.el
@@ -7,4 +7,13 @@
       magit-push-arguments (quote ("--set-upstream"))
       magit-save-repository-buffers (quote dontask))
 
+;; disable magit-insert-tags as it's a performance hog for magit-refs
+(setq magit-refs-sections-hook
+      '(magit-insert-error-header
+        magit-insert-branch-description
+        magit-insert-local-branches
+        magit-insert-remote-branches
+        ;; magit-insert-tags
+        ))
+
 (provide 'clgc-magit)
