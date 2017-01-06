92897c72e6e9c77702e4935998e4bd7c0dbc5140
Charles Comstock <dgtized@gmail.com>
2017-01-06 04:12:21 -0600

    remove debug messaging around toggle

--- site-lisp/bundle-outdated.el
+++ site-lisp/bundle-outdated.el
@@ -14,10 +14,8 @@
          (e (point-at-eol))
          (properties (get-text-property (1+ s) 'face)))
     (if properties
-        (progn
-          (message "remove")
-          (remove-text-properties s e '(face nil)))
-        (add-face-text-property s e '(:strike-through t)))))
+        (remove-text-properties s e '(face nil))
+      (add-face-text-property s e '(:strike-through t)))))
 
 (define-key text-mode-map (kbd "C-c u") 'bundle-outdated-update)
 (define-key text-mode-map (kbd "C-c i") 'bundle-outdated-toggle-strike)
