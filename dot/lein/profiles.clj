a5326b0383b588794e6ee698c7e553e24ee1f585
Charles Comstock <dgtized@gmail.com>
2017-04-17 23:40:05 -0500

    Add lein kibit for code suggestion/linter

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user
  {:plugins [[lein-vanity "0.2.0" :exclusions [org.clojure/clojure]]
             [lein-ancient "0.6.5"]
+            [lein-kibit "0.1.3"]
             [jonase/eastwood "0.2.0"]
             ;;[com.billpiel/sayid "0.0.10"]
             ]
