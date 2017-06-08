c6032b965a7fddfeeb1057f523656011eac76cf9
Charles Comstock <dgtized@gmail.com>
2017-06-08 12:18:46 -0500

    guard against toggle-monitor on alternate monitors

--- scripts/toggle-monitor.sh
+++ scripts/toggle-monitor.sh
@@ -3,7 +3,14 @@
 # FIXME: changing this with xrandr isn't sticky enough. After the monitor wakes
 # back up from sleep it keeps reverting to 1920x1080 until the setting is
 # adjusted with unity-control-center display settings again
-if xrandr | grep DP-0 | grep 3840x2160; then
+XRANDR=/tmp/xrandr.toggle-monitor
+xrandr | grep DP-0 > $XRANDR
+if [[ $? == 1 ]]; then
+    echo "DP-0 not found"
+    exit 1
+fi
+
+if grep 3840x2160 $XRANDR; then
     xrandr --output DP-0 --mode 1920x1080 --rate 60
 else
     xrandr --output DP-0 --mode 3840x2160 --rate 60
