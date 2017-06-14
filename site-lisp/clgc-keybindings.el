f6c8f011c8dafe76758f928c5b44aac6a4b0f6a3
Charles Comstock <dgtized@gmail.com>
2017-06-13 20:06:31 -0500

    add C-c V for crux-view-url in buffer

--- site-lisp/clgc-keybindings.el
+++ site-lisp/clgc-keybindings.el
@@ -105,6 +105,7 @@
 (global-set-key (kbd "C-c J") 'webjump)
 (global-set-key (kbd "C-c R") 'crux-rename-buffer-and-file)
 (global-set-key (kbd "C-c D") 'crux-delete-buffer-and-file)
+(global-set-key (kbd "C-c V") 'crux-view-url)
 
 (eval-after-load 'compile
   '(let ((map compilation-mode-map))
