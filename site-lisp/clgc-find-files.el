2ce1a92664182ab241b86ecf4d9a7fdfa9dc1d5c
Charles Comstock <dgtized@gmail.com>
2017-09-03 01:47:01 -0500

    add newly created winnow.el support to ag-mode

--- site-lisp/clgc-find-files.el
+++ site-lisp/clgc-find-files.el
@@ -48,6 +48,8 @@
       ag-group-matches nil
       ag-ignore-list '("*.min.js" "vendor/assets/*"))
 (add-hook 'ag-mode-hook 'next-error-follow-minor-mode)
+;; https://github.com/dgtized/winnow.el
+(add-hook 'ag-mode-hook 'winnow-mode)
 
 (require 'helm-config)
 
