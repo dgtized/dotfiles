37d556cb4c927c8355d4273440d16dfe7f5721e5
Charles Comstock <dgtized@gmail.com>
2018-02-10 16:26:56 -0600

    replace prodigy zeus tag with --simple-status check

--- site-lisp/clgc-prodigy.el
+++ site-lisp/clgc-prodigy.el
@@ -5,16 +5,9 @@
 
 (prodigy-define-tag
   :name 'zeus
-  :command "/bin/bash"
-  ;; Strip zeus start of color codes to prevent ansi out-of-bound errors
-  :args '("-c" "zeus start | sed \"s,\x1B\[[0-9;]*[a-zA-Z],,g\"")
-  :stop-signal 'sigkill
-  :kill-process-buffer-on-stop t
-  :on-output
-  (prodigy-callback (service)
-    (let ((poll-zeus "ps aux | grep -c 'zeus slave:'"))
-      (when (>= (string-to-number (shell-command-to-string poll-zeus)) 6)
-        (prodigy-set-status service 'ready)))))
+  :command "zeus"
+  :args '("--simple-status" "start")
+  :ready-message "test_helper status: ready")
 
 (prodigy-define-tag :name 'resque-pool
   :ready-message "Starting worker")
