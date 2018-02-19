6b045813f04d545e31f6ca5b2e43a26000f4a533
Charles Comstock <dgtized@gmail.com>
2018-02-18 22:17:35 -0600

    remove dimmer mode
    it was neat, but I generally want to read the other window and reduced contrast
    doesn't help.

--- site-lisp/clgc-init.el
+++ site-lisp/clgc-init.el
@@ -140,11 +140,6 @@
 (load custom-file 'noerror)
 (load-theme 'zenburn t) ;; use disable-theme / load-theme to switch
 
-;; reduce contrast on non-selected buffers
-(require 'dimmer)
-(setq dimmer-percent 0.10) ; really low, so mostly shows highlight shift
-(dimmer-activate)
-
 (defun clgc-set-font-size (size)
   "Change font size uniformly & on the fly"
   (interactive "nFont size in points: ")
