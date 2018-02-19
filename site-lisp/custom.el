c718c40365088c5df5b99771c0ce1af343ec81a8
Charles Comstock <dgtized@gmail.com>
2018-02-18 21:58:17 -0600

    enable some other themes and update quotation style

--- site-lisp/custom.el
+++ site-lisp/custom.el
@@ -4,25 +4,21 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(ediff-split-window-function (quote split-window-horizontally))
- '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+   '("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))
+ '(ediff-split-window-function 'split-window-horizontally)
+ '(ediff-window-setup-function 'ediff-setup-windows-plain)
  '(erc-nick "dgtized")
  '(git-commit-setup-hook
-   (quote
-    (git-commit-save-message git-commit-setup-changelog-support git-commit-turn-on-auto-fill git-commit-turn-on-flyspell git-commit-propertize-diff with-editor-usage-message)))
+   '(git-commit-save-message git-commit-setup-changelog-support git-commit-turn-on-auto-fill git-commit-turn-on-flyspell git-commit-propertize-diff with-editor-usage-message))
  '(git-commit-summary-max-length 60)
  '(global-magit-file-mode t)
  '(idle-update-delay 0.25)
  '(package-selected-packages
-   (quote
-    (material-theme hc-zenburn-theme leuven-theme dockerfile-mode ox-reveal org-download dimmer yard-mode package-lint winnow json-mode json-navigator lua-mode string-inflection aggressive-indent kibit-helper camcorder gnuplot gnuplot-mode helm-unicode terraform-mode atomic-chrome redis coverage request ob-elixir flycheck-dialyzer flycheck-elixir command-log-mode visual-fill-column zenburn-theme yari yaml-mode wgrep-ag web-server web-mode starter-kit-eshell solarized-theme smex smartscan smartparens slamhound simple-httpd sass-mode ruby-tools rubocop rspec-mode robe rinari rainbow-delimiters projectile-rails prodigy org-plus-contrib ob-http markdown-mode less-css-mode js2-mode iy-go-to-char ido-completing-read+ ibuffer-projectile helm-projectile helm-ag graphviz-dot-mode gitignore-mode github-clone github-browse-file gitconfig-mode gist fuzzy free-keys flycheck-elm flycheck-clojure flx-ido flash-region feature-mode expand-region evil epresent elm-mode diminish company-inf-ruby color-identifiers-mode coffee-mode clojure-snippets clojure-mode-extra-font-locking clojure-cheatsheet clj-refactor chruby bundler browse-kill-ring ag ace-window 4clojure)))
+   '(material-theme hc-zenburn-theme leuven-theme dockerfile-mode ox-reveal org-download dimmer yard-mode package-lint winnow json-mode json-navigator lua-mode string-inflection aggressive-indent kibit-helper camcorder gnuplot gnuplot-mode helm-unicode terraform-mode atomic-chrome redis coverage request ob-elixir flycheck-dialyzer flycheck-elixir command-log-mode visual-fill-column zenburn-theme yari yaml-mode wgrep-ag web-server web-mode starter-kit-eshell solarized-theme smex smartscan smartparens slamhound simple-httpd sass-mode ruby-tools rubocop rspec-mode robe rinari rainbow-delimiters projectile-rails prodigy org-plus-contrib ob-http markdown-mode less-css-mode js2-mode iy-go-to-char ido-completing-read+ ibuffer-projectile helm-projectile helm-ag graphviz-dot-mode gitignore-mode github-clone github-browse-file gitconfig-mode gist fuzzy free-keys flycheck-elm flycheck-clojure flx-ido flash-region feature-mode expand-region evil epresent elm-mode diminish company-inf-ruby color-identifiers-mode coffee-mode clojure-snippets clojure-mode-extra-font-locking clojure-cheatsheet clj-refactor chruby bundler browse-kill-ring ag ace-window 4clojure))
  '(smie-indent-basic 2)
  '(web-mode-markup-indent-offset 2)
  '(yas-prompt-functions
-   (quote
-    (yas-ido-prompt yas-x-prompt yas-dropdown-prompt yas-completing-prompt yas-no-prompt))))
+   '(yas-ido-prompt yas-x-prompt yas-dropdown-prompt yas-completing-prompt yas-no-prompt)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
