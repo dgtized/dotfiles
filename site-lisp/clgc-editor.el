67c58840acc0940bcf0b9ded72ae30ff39664540
Charles Comstock <dgtized@gmail.com>
2018-04-06 00:19:27 -0500

    try prioritizing dabbrev for hippie expand first

--- site-lisp/clgc-editor.el
+++ site-lisp/clgc-editor.el
@@ -75,6 +75,18 @@
 
 (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
 
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-all-abbrevs
+        try-expand-list
+        try-expand-line
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
+
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 (global-flycheck-mode)
 
