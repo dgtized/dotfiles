7b83b0364b19c7bb810a37853c8884fe061d7f22
Charles Comstock <dgtized@gmail.com>
2018-05-11 11:14:28 -0500

    add synausaurus and wordnet

--- utopic-install.sh
+++ utopic-install.sh
@@ -10,6 +10,7 @@ sudo aptitude install emacs-snapshot emacs-snapshot-el vim \
      git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties \
      graphviz dconf-cli gnome-tweak-tool \
      p7zip-full tree parallel pv gawk netcat-openbsd jq \
+     wordnet \
      lm-sensors htop iotop mytop linux-tools-common linux-tools-generic valgrind \ # perf
      default-jdk visualvm golang-go racket sbcl rlwrap \
      gitk \
