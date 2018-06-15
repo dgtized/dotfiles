594a29fa3a8788794fb73da4c4d9911af12230a4
Charles Comstock <dgtized@gmail.com>
2018-06-15 16:05:12 -0500

    upgrade eastwood/lein-ancient

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -1,8 +1,8 @@
 {:user
  {:plugins [[lein-vanity "0.2.0" :exclusions [org.clojure/clojure]]
-            [lein-ancient "0.6.14"]
+            [lein-ancient "0.6.15"]
             [lein-kibit "0.1.6"]
-            [jonase/eastwood "0.2.5"]
+            [jonase/eastwood "0.2.6"]
             ;;[com.billpiel/sayid "0.0.10"]
             [lein-cljfmt "0.5.7"]]
   :dependencies [[slamhound "1.5.5"]]
