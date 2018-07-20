469231c5c383afc91743fe6bea7c96fd7948ace0
Charles Comstock <dgtized@gmail.com>
2018-07-20 18:25:23 -0500

    version bump cljfmt & eastwood

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -2,8 +2,8 @@
  {:plugins [[lein-vanity "0.2.0" :exclusions [org.clojure/clojure]]
             [lein-ancient "0.6.15"]
             [lein-kibit "0.1.6"]
-            [jonase/eastwood "0.2.6"]
+            [jonase/eastwood "0.2.8"]
             ;;[com.billpiel/sayid "0.0.10"]
-            [lein-cljfmt "0.5.7"]]
+            [lein-cljfmt "0.6.0"]]
   :dependencies [[slamhound "1.5.5"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
