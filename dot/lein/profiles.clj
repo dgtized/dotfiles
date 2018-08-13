44bcae569f8e039f2bdc9fc83ae39f0cf8d2ee09
Charles Comstock <dgtized@gmail.com>
2018-08-13 00:38:10 -0500

    use eastwood 0.2.9

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -2,7 +2,7 @@
  {:plugins [[lein-vanity "0.2.0" :exclusions [org.clojure/clojure]]
             [lein-ancient "0.6.15"]
             [lein-kibit "0.1.6"]
-            [jonase/eastwood "0.2.8"]
+            [jonase/eastwood "0.2.9"]
             ;;[com.billpiel/sayid "0.0.10"]
             [lein-cljfmt "0.6.0"]]
   :dependencies [[slamhound "1.5.5"]]
