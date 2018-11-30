f6ee2edbe7944c8537e2e383efda7a035f3de5e9
Charles Comstock <dgtized@gmail.com>
2018-11-30 16:47:13 -0600

    lein-cljfmt 0.6.2

--- dot/lein/profiles.clj
+++ dot/lein/profiles.clj
@@ -6,6 +6,6 @@
             [lein-pprint "1.2.0"]
             [jonase/eastwood "0.3.3"]
             [com.billpiel/sayid "0.0.17"]
-            [lein-cljfmt "0.6.1"]]
+            [lein-cljfmt "0.6.2"]]
   :dependencies [[slamhound "1.5.5"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
