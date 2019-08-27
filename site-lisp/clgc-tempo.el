50ea40a49ae653d6d6a2818e11676cd023337c5b
Charles Comstock <dgtized@gmail.com>
2019-08-26 22:55:14 -0500

    Reformat template for org-digraph

--- site-lisp/clgc-tempo.el
+++ site-lisp/clgc-tempo.el
@@ -2,10 +2,13 @@
 ;; (setq tempo-interactive t)
 
 (tempo-define-template "org-digraph"
-                       '(> "#+begin_src dot :file " (p "Filename: ") ".png\n"
-                           > "digraph G {\n"
-                           > "}\n"
-                           > "#+end_src\n"))
+                       '(> "#+begin_src dot :file " (p "Filename:") ".png" n
+                           > "digraph G {" n
+                           > r> n>
+                           "}" n
+                           > "#+end_src" n)
+                       "org-digraph"
+                       "Insert a dot digraph src block with filename")
 
 (define-abbrev org-mode-abbrev-table "dotgraph" "" 'tempo-template-org-digraph)
 
