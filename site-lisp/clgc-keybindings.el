ad9690906f98ea7b938453befc78047644e90053
Charles Comstock <dgtized@gmail.com>
2019-10-21 00:01:50 -0500

    Enable ruby-hash-syntax for toggling 1.8 to 1.9 style

--- site-lisp/clgc-keybindings.el
+++ site-lisp/clgc-keybindings.el
@@ -217,6 +217,7 @@
          (define-key ruby-mode-map (kbd "C-c v") 'chruby-use-corresponding)
          (define-key ruby-mode-map (kbd "S-<f6>") 'coverage-mode)
          (define-key ruby-mode-map (kbd "C-c :") 'clgc-ruby-string->symbol)
+         (define-key ruby-mode-map (kbd "C-c #") 'ruby-toggle-hash-syntax)
          (define-key ruby-mode-map (kbd "C-c C-u") 'string-inflection-ruby-style-cycle)))
 
 (with-eval-after-load 'rspec-dired-mode
