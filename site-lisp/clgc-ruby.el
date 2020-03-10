a4d09a8c72038b7f0191f2ea1b9289e2a9a37306
Charles Comstock <dgtized@gmail.com>
2020-03-10 12:24:44 -0500

    Update rspec to be chruby aware

--- site-lisp/clgc-ruby.el
+++ site-lisp/clgc-ruby.el
@@ -51,7 +51,7 @@
          (setq rspec-command-options "--format documentation --profile 30"
                rspec-use-bundler-when-possible nil
                rspec-use-opts-file-when-available nil
-               rspec-use-rvm t
+               rspec-use-chruby t
                rspec-primary-source-dirs '("app"))))
 
 (eval-when-compile (require 'feature-mode))
