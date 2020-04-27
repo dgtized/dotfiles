415b9c91a9798e3adfade3fdc616ab34fda30281
Charles Comstock <dgtized@gmail.com>
2020-04-27 02:14:14 -0500

    run chrome stable if beta is unavailable

--- scripts/gnome-xinit.sh
+++ scripts/gnome-xinit.sh
@@ -9,6 +9,12 @@ source "$HOME/.bashrc"
 # initial apps
 uxterm -e "tmux -2 new-session -A -s default" &
 "$HOME/usr/bin/ec" -c &
-google-chrome-beta &
+
+if test -e /usr/bin/google-chrome-beta ; then
+    google-chrome-beta &
+else
+    google-chrome-stable &
+fi
+
 slack &
 
