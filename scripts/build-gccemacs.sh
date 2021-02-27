cbdb0c9069edb7926689bdd8ed5cdbca78ef9974
Charles Comstock <dgtized@gmail.com>
2021-02-27 11:27:51 -0600

    Flag name changed to `native-compilation` from `nativecomp`

--- scripts/build-gccemacs.sh
+++ scripts/build-gccemacs.sh
@@ -34,7 +34,7 @@ fi
     ./configure --build=x86_64-linux-gnu \
      --prefix="$HOME/usr" --program-transform-name='s/^emacs/gccmacs/' \
      --disable-silent-rules --with-modules=yes --with-x=yes --with-x-toolkit=gtk3 --with-xwidgets=yes \
-     --with-nativecomp --with-mailutils \
+     --with-native-compilation --with-mailutils \
      'CFLAGS=-g -O2 -fstack-protector-strong -Wformat -Werror=format-security' \
      'CPPFLAGS=-Wdate-time -D_FORTIFY_SOURCE=2' \
      'LDFLAGS=-Wl,-Bsymbolic-functions -Wl,-z,relro'
