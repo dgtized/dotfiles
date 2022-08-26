97b7093d459fc892b7a3ac25ff48ef88bea4b0c8
Charles Comstock <dgtized@gmail.com>
2022-08-26 06:51:18 -0500

    no longer supporting focal 20.04

--- scripts/build-gccemacs.sh
+++ scripts/build-gccemacs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # outline from https://www.masteringemacs.org/article/speed-up-emacs-libjansson-native-elisp-compilation
-# assumes install in 20.04 focal
 
 mkdir -p ~/code/upstream
 pushd ~/code/upstream
@@ -15,21 +14,9 @@ pushd emacs
 git checkout master
 git pull --rebase
 
-focal=0
-if grep -c focal /etc/lsb-release; then
-    focal=1
-    export CC="gcc-10"
-fi
-
 # alternative use versions from add-apt-repository ppa:ubuntu-toolchain-r/ppa
 
-if [[ $focal == 1 ]]; then
-    sudo apt install -y gcc-10 libgccjit0 libgccjit-10-dev
-else
-    sudo apt install -y libgccjit0 libgccjit-11-dev
-fi
-
-sudo apt install -y libjansson4 libjansson-dev
+sudo apt install -y libgccjit0 libgccjit-11-dev libjansson4 libjansson-dev
 sudo apt build-dep -y emacs-snapshot
 
 JOBS=$(($(grep -c processor /proc/cpuinfo) - 2))
