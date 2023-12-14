c6dd1506446999fc40a75aeae5b80e365dd39abf
Charles Comstock <dgtized@gmail.com>
2023-12-14 14:11:06 -0600

    babashka 1.3.186

--- utopic-install.sh
+++ utopic-install.sh
@@ -176,7 +176,7 @@ curl -sLO https://raw.githubusercontent.com/borkdude/clj-kondo/master/script/ins
 
 # babashka
 # download and unpack from https://github.com/borkdude/babashka/releases
-(export BV="1.3.185" &&
+(export BV="1.3.186" &&
      wget https://github.com/babashka/babashka/releases/download/v${BV}/babashka-${BV}-linux-amd64.tar.gz -O babashka.tgz) &&
     tar -zxf babashka.tgz && mv -vf bb ~/usr/bin && rm -vf babashka.tgz
 
